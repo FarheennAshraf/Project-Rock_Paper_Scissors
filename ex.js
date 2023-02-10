730506548e009616ee6c854c5ced3c6ab7c8dd14
farheen <78275998+FarheennAshraf@users.noreply.github.com>
2023-02-10 15:53:41 +0530

    Add Pseudocode to js file

---
+++ ex.js
@@ -0,0 +1,12 @@
+// #Define an array with valid input values 'Rock', 'Paper', 'Scissors'
+// #Define a function getComputerChoice that returns a random value either 'Rock', 'Paper', 'Scissors'
+// #Call the getComputer and store the returned value in computerSelection variable
+// #Take user input from the prompt and store it in playerSelection variable. [Make it case insensitive]
+// #Define another function that plays a single round of rock, paper, scissors and returns a string.
+// #If the player input and computer input are equal, it is a tie
+// #Else 
+// #If the player input is 'Rock' & computer input is 'Scissors', Player wins; else Player loses.
+// #ElseIf the player input is 'Scissors' & computer input is 'Paper', Player wins; else Player loses.
+// #ElseIf the player input is 'Paper' & computer input is 'Rock', Player wins; else Player loses.
+// #Else the player input is invalid 
+// #Call function passing the player input as playerSelection and computer input as computerSelection.
