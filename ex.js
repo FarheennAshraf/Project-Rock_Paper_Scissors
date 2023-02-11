2824f0ea688da404d549e3ac4a86afb7b14ebbe7
farheen <78275998+FarheennAshraf@users.noreply.github.com>
2023-02-11 10:48:53 +0530

    Update singleRound(), Add game(), update code
    rename singleRound() with playRound() and add console.log() to display
    the computer & player selection in the func, update parameters to receive
    values at the time of func call and add the condition for null value of
    player selection. Add 5 round play code to game() func. Update the
    counter comparison code to add logic for equality. Make changes in
    comments.

--- ex.js
+++ ex.js
@@ -14,94 +14,94 @@ function getPlayerChoice(){
     return playerSelection;
 }
 
+//updated: Add a counter to maintain score;
+let counterComp = 0;
+let counterPlayer = 0;
 
-// #Define a function that plays a single round of rock, paper, scissors and returns a string.
+
+// #Define a function playRound() that plays a single round of rock, paper, scissors and returns a string.
 // #If the player input and computer input are equal, it is a tie. Case-insensitive comparison.
 // #Else 
 // #If the player input is 'Rock' & computer input is 'Scissors', Player wins; else Player loses.
 // #ElseIf the player input is 'Scissors' & computer input is 'Paper', Player wins; else Player loses.
 // #ElseIf the player input is 'Paper' & computer input is 'Rock', Player wins; else Player loses.
 // #Else the player input is invalid.
-
-//update: Add a counter to maintain score;
-let counterComp = 0;
-let counterPlayer = 0;
-function singleRound(computerSelection, playerSelection){
-    if(computerSelection.toLowerCase() === playerSelection.toLowerCase()){
-        return "It's a tie!";
-    }
-    else{
-        if(playerSelection.toLowerCase() === 'rock'){
-            if(computerSelection.toLowerCase() === 'scissors'){
-                //Add counter, increment if player wins
-                counterPlayer++;
-                return `You win! ${playerSelection} beats ${computerSelection}`;
-            }
-            else{
-                //Add counter, increment if computer wins
-                counterComp++;
-                return `You lose! ${computerSelection} beats ${playerSelection}`;
-            }
-        }
-        else if(playerSelection.toLowerCase() === 'paper'){
-            if(computerSelection.toLowerCase() === 'rock'){
-                counterPlayer++;
-                return `You win! ${playerSelection} beats ${computerSelection}`;
-            }
-            else{
-                counterComp++;
-                return `You lose! ${computerSelection} beats ${playerSelection}`;
-                
-            }
-        }
-        else if(playerSelection.toLowerCase() === 'scissors'){
-            if(computerSelection.toLowerCase() === 'paper'){
-                counterPlayer++;
-                return `You win! ${playerSelection} beats ${computerSelection}`;
-            }
-            else{
-                counterComp++;
-                return `You lose! ${computerSelection} beats ${playerSelection}`;
-            }
+function playRound(computerSelection = getComputerChoice(), playerSelection = getPlayerChoice()){
+    if(playerSelection){
+        console.log(`Player's choice: ${playerSelection}`);
+        console.log(`Computer's choice: ${computerSelection}`);
+        if(computerSelection.toLowerCase() === playerSelection.toLowerCase()){
+            return "It's a tie!";
         }
         else{
-            return "Enter a valid choice!";
-        }
-    }
-}
-// Play Single Round Game:
-// #Call the getComputerChoice() and store the returned value in computerSelection variable
-// const computerSelection = getComputerChoice();
-// console.log(computerSelection);
+            if(playerSelection.toLowerCase() === 'rock'){
+                if(computerSelection.toLowerCase() === 'scissors'){
+                    //Add counter, increment if player wins
+                    counterPlayer++;
+                    return `You win! ${playerSelection} beats ${computerSelection}`;
+                }
+                else{
+                    //Add counter, increment if computer wins
+                    counterComp++;
+                    return `You lose! ${computerSelection} beats ${playerSelection}`;
+                }
+            }
+            else if(playerSelection.toLowerCase() === 'paper'){
+                if(computerSelection.toLowerCase() === 'rock'){
+                    counterPlayer++;
+                    return `You win! ${playerSelection} beats ${computerSelection}`;
+                }
+                else{
+                    counterComp++;
+                    return `You lose! ${computerSelection} beats ${playerSelection}`;                
+                }
+            }
+            else if(playerSelection.toLowerCase() === 'scissors'){
+                if(computerSelection.toLowerCase() === 'paper'){
+                    counterPlayer++;
+                    return `You win! ${playerSelection} beats ${computerSelection}`;
+                }
+                else{
+                    counterComp++;
+                    return `You lose! ${computerSelection} beats ${playerSelection}`;
+                }
+            }
+            else{
+                return "Please enter a valid choice!";
+            }
+        }    
+    } 
+    else{
+        return "Please enter a valid choice";
+    } 
+ }
 
-// #Take input from the user
-// const playerSelection = prompt("Enter your choice:");
-// console.log(playerSelection);
-
-// #Call function passing the functions: getComputerChoice() & getPlayerChoice(), that return inputs, as parameters.
-// console.log(singleRound(computerSelection, playerSelection));
 
 
 // #Play 5 Round Game:
-// #1. Add a new function game(). call singleRound() inside for loop to iterate 5 times. Display results of each round.
-// #2. Get computerSelection & playerSelection for each iteration i.e 5 times.
-for(let i=1; i<=5; i++){
-    const computerSelection = getComputerChoice();
-    const playerSelection = getPlayerChoice();
-    console.log(playerSelection);
-    console.log(computerSelection);
-    console.log(singleRound(computerSelection,playerSelection));
-    // #3. Keep a score in each iteration. Add each counter for computer and player and increment it for the one who wins.
-    console.log("Computer : "+counterComp);
-    console.log("Player : "+counterPlayer);
-}
+// #1. Add a new function game(). call playRound() inside for loop to iterate 5 times. Display results of each round.
+// #2. Get computerSelection & playerSelection for each iteration i.e 5 times. (not needed, call the functions to get each selection in playRound() parameters.)
+function game(){
+    for(let i=1; i<=5; i++){
+        // const computerSelection = getComputerChoice();
+        // const playerSelection = getPlayerChoice();
+        console.log(playRound(computerSelection,playerSelection));
+        // #3. Keep a score in each iteration. Add each counter for computer and player and increment it for the one who wins.
+        console.log("Computer : "+counterComp);
+        console.log("Player : "+counterPlayer);
+    }
+    // #4. Display the score
+    console.log(`Score:  Computer-${counterComp} : Player-${counterPlayer}`);
 
-// #4. Display the score
-console.log(`Score -  Computer_${counterComp}: Player_${counterPlayer}`);
-
-// #3. Compare the counters, the greater wins. return a string stating winner or loser at end.
-if(counterComp > counterPlayer){
+    // #5. Compare the counters, the greater wins. If equal it is a tie return a string stating winner or loser at end.
+    if(counterComp > counterPlayer){
     console.log("You lose! Computer wins!");
-} else{
+    } 
+    else if(counterComp < counterPlayer){
     console.log("You win! Computer loses!");
+    } else{
+        console.log("It is a Tie!");
+    }
 }
+
+
